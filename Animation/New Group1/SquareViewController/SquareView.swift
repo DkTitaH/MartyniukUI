11396a9e13c93fae7830d960ba21c90434f54729
Владимир Мартынюк <dktitah@gmail.com>
2019-01-04 14:31:34 +0200

    SquareView -> func renaming

--- Animation/New Group1/SquareViewController/SquareView.swift
+++ Animation/New Group1/SquareViewController/SquareView.swift
@@ -15,13 +15,13 @@ class SquareView: UIView {
         case Stop
     }
     
-    
     @IBOutlet var startButton: UIButton?
     
     @IBOutlet var isAnimated: UISwitch?
     
     @IBAction func onSwitch(_ sender: Any) {
         self.isMoving = false
+        self.startButton?.setTitle(State.Start.rawValue, for: .normal)
     }
     
     var isCancelled: Bool {
@@ -36,7 +36,7 @@ class SquareView: UIView {
         let origin = self.frame.inset(by: self.safeAreaInsets).topLeft
         let rect = CGRect(origin: origin, size: CGSize(width: 100, height: 100))
         let square = Square(frame: rect)
-        square.backgroundColor = .green
+        square.backgroundColor = .blue
 
         return square
     }()
@@ -47,6 +47,7 @@ class SquareView: UIView {
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        
         self.addSubview(self.square)
     }
     
@@ -55,22 +56,22 @@ class SquareView: UIView {
         
         if self.isMoving && !self.animated {
             self.startButton?.setTitle(State.Stop.rawValue, for: .normal)
-            self.setPositionAnimate(duration: self.animationDuration, animated: self.isAnimated?.isOn ?? false)
+            self.setPosition(duration: self.animationDuration, animated: self.isAnimated?.isOn ?? false)
         } else {
             self.startButton?.setTitle(State.Start.rawValue, for: .normal)
         }
     }
     
-    private func setPositionAnimate(duration: TimeInterval,animated: Bool) {
+    private func setPosition(duration: TimeInterval,animated: Bool) {
         UIView.animate(
             withDuration: animated ? 1 : 0,
             animations: {
                 self.animated = true
-                self.setPosition()
+                self.changePosition()
             },
             completion: { bool in
                 if self.isMoving {
-                    self.setPositionAnimate(duration: duration, animated: animated)
+                    self.setPosition(duration: duration, animated: animated)
                 } else {
                     self.animated = false
                 }
@@ -78,7 +79,7 @@ class SquareView: UIView {
         )
     }
 
-    private func setPosition() {
+    private func changePosition() {
         self.position = self.generator.next()
         let positions = self.positions()
         
